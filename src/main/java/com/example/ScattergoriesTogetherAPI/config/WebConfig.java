bc7442179967c2fea8745556c95f168773a731f2
merlyngilchrist <157403080+merlyngilchrist@users.noreply.github.com>
2024-11-30 15:54:23 -0700

    Update WebConfig.java

--- src/main/java/com/example/ScattergoriesTogetherAPI/config/WebConfig.java
+++ src/main/java/com/example/ScattergoriesTogetherAPI/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/**").allowedOrigins("romacosta.github.io").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); //Adjust for frontend's URL
+                registry.addMapping("/**").allowedOrigins("https://romacosta.github.io").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true); //Adjust for frontend's URL
             }
         };
     }
