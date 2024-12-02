1a146f669844fdb7b9590882f6fbbb2b06869477
merlyngilchrist <157403080+merlyngilchrist@users.noreply.github.com>
2024-12-02 13:53:57 -0700

    Update WebSocketSessionManager.java

--- src/main/java/com/example/ScattergoriesTogetherAPI/utility/WebSocketSessionManager.java
+++ src/main/java/com/example/ScattergoriesTogetherAPI/utility/WebSocketSessionManager.java
@@ -3,8 +3,10 @@ package com.example.ScattergoriesTogetherAPI.utility;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
+@Component
 public class WebSocketSessionManager {
     private final Map<String, WebSocketSession> userSessions = new ConcurrentHashMap<>();
 
