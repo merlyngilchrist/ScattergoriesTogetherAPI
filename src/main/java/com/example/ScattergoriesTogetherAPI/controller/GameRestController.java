02e576480da6030efe7db3cf1f382a7f8966c228
merlyngilchrist <157403080+merlyngilchrist@users.noreply.github.com>
2024-12-03 17:24:47 -0700

    Update GameRestController.java

--- src/main/java/com/example/ScattergoriesTogetherAPI/controller/GameRestController.java
+++ src/main/java/com/example/ScattergoriesTogetherAPI/controller/GameRestController.java
@@ -75,8 +75,6 @@ public class GameRestController {
 
     @PostMapping("/{gameCode}/join")
     public ResponseEntity<?> joinGame(@PathVariable String gameCode, @RequestBody Map<String, String> payload){
-        System.out.println("Game Code: " + gameCode);
-        System.out.println("Payload: " + payload);
         String username = payload.get("username");
         if (username == null || username.isEmpty()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is required");
